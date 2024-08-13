42f05e77fda6f574ebe21b09dd4cc416c138071b
MinHieu2811 <cuunhatnhat51@gmail.com>
2024-08-13 22:49:41 +0700

    update pie chart

--- src/components/custom/CustomPieChart.tsx
+++ src/components/custom/CustomPieChart.tsx
@@ -9,56 +9,69 @@ const CustomPieChart = ({ totalSpent, totalBudget }: CustomPieChartProps) => {
   return (
     <>
       <div className="flex justify-center items-center">
-        <div
-          className="pie"
-          pie-data={(totalSpent / totalBudget) * 100}
-          pie-width={"10px"}
-          pie-color={"purple"}
-        ></div>
+        <div className="pie animate"></div>
       </div>
-      <style>
-        {`
-        .pie {
-  width: 150px;
+      {typeof window !== "undefined" && (
+        <style>
+          {`
+        @property --p{
+  syntax: '<number>';
+  inherits: true;
+  initial-value: 1;
+}
+
+.pie {
+  --p:${(totalSpent / totalBudget) * 100};
+  --b:15px;
+  --c: ${(totalSpent / totalBudget) * 100 < 100 ? "#2563eb" : "red"};
+  --w:200px;
+
+  width: var(--w);
   aspect-ratio: 1;
   position: relative;
   display: inline-grid;
-  place-content: center;
   margin: 5px;
+  place-content: center;
   font-size: 25px;
   font-weight: bold;
   font-family: sans-serif;
 }
-.pie:before {
+.pie:before,
+.pie:after {
   content: "";
   position: absolute;
   border-radius: 50%;
+}
+.pie:before {
   inset: 0;
-  background: conic-gradient(red ${(totalSpent / totalBudget)}turn,#b1b0b0 0turn);
-  -webkit-mask: radial-gradient(
-    farthest-side,
-    #0000 calc(99% - 10px),
-    #000 calc(100% - 10px)
-  );
-  mask: radial-gradient(
-    farthest-side,
-    #0000 calc(99% - 10px),
-    #000 calc(100% - 10px)
-  );
+  background:
+    radial-gradient(farthest-side,var(--c) 98%,#0000) top/var(--b) var(--b) no-repeat,
+    conic-gradient(var(--c) calc(var(--p)*1%),#d6d2d2 0);
+  -webkit-mask: radial-gradient(farthest-side,#0000 calc(99% - var(--b)),#000 calc(100% - var(--b)));
+          mask: radial-gradient(farthest-side,#0000 calc(99% - var(--b)),#000 calc(100% - var(--b)));
 }
 .pie:after {
-  --b: 10px;
-  --c: "red";
-  --p: 60;
-  content: "";
-  position: absolute;
-  border-radius: 50%;
   inset: calc(50% - var(--b)/2);
   background: var(--c);
-  transform: rotate(calc(${(totalSpent / totalBudget)}*3.6deg)) translateY(calc(50% - 150px/2));
+  transform: rotate(calc(var(--p)*3.6deg)) translateY(calc(50% - var(--w)/2));
+}
+.animate {
+  animation: p 1s 0.5s both;
+}
+.no-round:before {
+  background-size: 0 0, auto;
+}
+.no-round:after {
+  content: none;
+}
+@keyframes p{
+  from{--p:0}
+}
+
       `}
-        `
-      </style>
+          `
+        </style>
+      )}
     </>
   );
 };
--- src/pages/budget/my-budget.tsx
+++ src/pages/budget/my-budget.tsx
@@ -2,25 +2,86 @@ import { getServerSession, NextAuthOptions } from "next-auth";
 import { authOptions } from "../api/auth/[...nextauth]";
 import { GetServerSideProps } from "next";
 import { MemoPieChart } from "@/components/custom/CustomPieChart";
+import { useSession } from "next-auth/react";
+import { Inter } from "next/font/google";
+import { StickyHeader } from "@/components/custom";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Separator } from "@/components/ui/separator";
+import { addMonths, differenceInCalendarDays, startOfMonth } from "date-fns";
 
 type Budget = {
-    id?: string,
-    category?: string,
-    budget?: number,
-    spent?: number
-}
+  id?: string;
+  category?: string;
+  budget?: number;
+  spent?: number;
+};
 
 type MyBudgetProps = {
-  listBudget: Budget[]
-  totalSpent: number
-  totalBudget: number
-}
+  listBudget: Budget[];
+  totalSpent: number;
+  totalBudget: number;
+};
 
-const MyBudget = ({totalBudget, totalSpent}: MyBudgetProps) => {
+const inter = Inter({ subsets: ["latin"] });
+
+const MyBudget = ({ totalBudget, totalSpent }: MyBudgetProps) => {
+  const { data: session } = useSession();
   return (
-    <MemoPieChart totalBudget={totalBudget} totalSpent={totalSpent} />
-  )
-}
+    <main className={`min-h-screen ${inter.className}`}>
+      <StickyHeader
+        imgAva={session?.user?.image || ""}
+        email={session?.user?.email || ""}
+        name={session?.user?.name || ""}
+      />
+      <div className="py-2 px-2">
+        <Card className="">
+          <CardHeader>
+            <CardTitle className="text-2xl mb-1 text-center">
+              My Budget
+            </CardTitle>
+          </CardHeader>
+          <CardContent>
+            <MemoPieChart totalBudget={totalBudget} totalSpent={totalSpent} />
+          </CardContent>
+          <CardDescription>
+            <div className="flex items-center flex-col">
+              <Separator />
+              <div className="flex flex-col items-center py-2">
+                <div className="text-lg">
+                  {Number(totalBudget)?.toLocaleString()} VND
+                </div>
+                <div className="text-lg">Total Budget</div>
+              </div>
+              <Separator />
+              <div className="flex flex-col items-center py-2">
+                <div className="text-lg">
+                  {Number(totalSpent)?.toLocaleString()} VND
+                </div>
+                <div className="text-lg">Total Spent</div>
+              </div>
+              <Separator />
+              <div className="flex flex-col items-center py-2">
+                <div className="text-lg">
+                  {differenceInCalendarDays(
+                    startOfMonth(addMonths(new Date(), 1)),
+                    new Date()
+                  )}
+                </div>
+                <div className="text-lg">days left</div>
+              </div>
+            </div>
+          </CardDescription>
+        </Card>
+      </div>
+    </main>
+  );
+};
 
 export const getServerSideProps = (async (context) => {
   const session = await getServerSession(
@@ -43,34 +104,34 @@ export const getServerSideProps = (async (context) => {
         ? "http://localhost:3000"
         : "https://money-lover-omega.vercel.app";
 
-    const recentTransaction = await fetch(
-      `${baseURL}/api/google/budget/get`,
-      {
-        method: "GET",
-        headers: {
-          Cookie: context.req.headers.cookie || "",
-        },
-      }
-    );
+    const recentTransaction = await fetch(`${baseURL}/api/google/budget/get`, {
+      method: "GET",
+      headers: {
+        Cookie: context.req.headers.cookie || "",
+      },
+    });
 
     const { budgetRes } = await recentTransaction?.json();
 
-    const total = budgetRes?.reduce((acc: {totalSpent: number, totalBudget: number}, curr: Budget) => {
-      return {
-        totalSpent: acc.totalSpent + Number(curr?.spent || "0"),
-        totalBudget: acc.totalBudget + Number(curr.budget || "0")
+    const total = budgetRes?.reduce(
+      (acc: { totalSpent: number; totalBudget: number }, curr: Budget) => {
+        return {
+          totalSpent: acc.totalSpent + Number(curr?.spent || "0"),
+          totalBudget: acc.totalBudget + Number(curr.budget || "0"),
+        };
+      },
+      {
+        totalSpent: 0,
+        totalBudget: 0,
       }
-    }, {
-      totalSpent: 0,
-      totalBudget: 0
-    })
+    );
 
     return {
       props: {
         session,
         listBudget: budgetRes || [],
         totalSpent: total?.totalSpent || 0,
-        totalBudget: total?.totalBudget || 0
+        totalBudget: total?.totalBudget || 0,
       },
     };
   } catch (error) {
@@ -80,11 +141,10 @@ export const getServerSideProps = (async (context) => {
         session,
         listBudget: [],
         totalBudget: 0,
-        totalSpent: 0
+        totalSpent: 0,
       },
     };
   }
 }) satisfies GetServerSideProps<MyBudgetProps>;
 
-
-export default MyBudget
+export default MyBudget;
